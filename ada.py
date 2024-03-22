70d7ddbc7dfc09df2bd7ba88624043e16ffba2c8
delfidome <162723242+delfidome@users.noreply.github.com>
2024-03-22 12:56:06 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name = first_name.lower()  + " " + last_name.lower()
+    print(full_name)
+    print(full_name.title())
+    print(full_name.upper())
+    print(f"\t{full_name}")
+
+ada()
