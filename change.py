699f709fed87e492499e3e125f5386de7304e02f
delfidome <162723242+delfidome@users.noreply.github.com>
2024-03-23 20:14:07 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,20 @@
 def change():
     expense = 23.75
     money = 100
+    print (f"Ingresar gasto:")
+    print (expense)
+    print (f"Dinero recibido:")
+    print (money)
+    print("\n")
+    print (f"Vuelto:")
+    change = (money-expense)
+    print (change)
+    print ("\n")
+    print (f"Pesos:")
+    print (int (change))
+    centavos = (change - int (change)) * (int (money))
+    print (f"Centavos:")
+    print (int (centavos))
+
+
+change ()
