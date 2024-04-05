19a9c1aae9ccde7b44a783c48c11c3e3999b9e81
delfidome <162723242+delfidome@users.noreply.github.com>
2024-04-05 09:02:12 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,8 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    print (f"the result of X comes first in the dictionary than Y is {x < y }") 
+    print (f"the result of Y comes first in the dictionary than X is {x > y}")
+    
+
+earth ()
